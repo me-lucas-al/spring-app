97dfeff4ef2ae33d695616c4af2a4b569139c120
me-lucas-al <me.lucasalmeida@gmail.com>
2026-03-16 23:42:44 -0300

    feat: init project

---
+++ src/test/java/com/example/login_auth_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
